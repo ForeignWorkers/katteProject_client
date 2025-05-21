96bceda4105babd12516ea633c1a77d47bc796e4
minquu <jeongmingyu@jeongmingyuui-MacBookPro.local>
2025-05-21 11:45:59 +0900

    인텔리제이로 스프링 부트 프로젝트 세팅해서 넣기
    빌드 후 테스트 완료

---
+++ katte_project_client/src/main/java/me/soldesk/katte_project_client/KatteProjectClientApplication.java
@@ -0,0 +1,13 @@
+package me.soldesk.katte_project_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatteProjectClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatteProjectClientApplication.class, args);
+    }
+
+}
---
+++ katte_project_client/src/test/java/me/soldesk/katte_project_client/KatteProjectClientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.soldesk.katte_project_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatteProjectClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
