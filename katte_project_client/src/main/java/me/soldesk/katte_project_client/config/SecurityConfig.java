6053d4c6c3444270d21ca90dace9ba54d77b899a
minquu <minquu@gmail.com>
2025-06-10 14:42:27 +0900

    시큐리티 업로드

--- katte_project_client/src/main/java/me/soldesk/katte_project_client/config/SecurityConfig.java
+++ katte_project_client/src/main/java/me/soldesk/katte_project_client/config/SecurityConfig.java
@@ -18,23 +18,36 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(basic -> basic.disable())
-                .formLogin(form -> form
-                        .loginPage("/html/Loginpage/Login.html")
-                        .defaultSuccessUrl("/html/Mainpage/Mainpage.html", true)
-                        .permitAll()
-                )
+                .formLogin(form -> form.disable()) // 로그인 폼도 막기
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/",                     // 메인 접근 허용
-                                "/html/Mainpage/Mainpage.html",   // 리다이렉트 대상 허용
-                                "/html/Loginpage/Login.html",     // 로그인 페이지 허용
-                                "/html/Signuppage/Signup.html",
-                                "/css/**", "/js/**",
-                                "/signUp",
-                                "/signUp_pro"
-                        ).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 );
+
         return http.build();
     }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .httpBasic(basic -> basic.disable())
+//                .formLogin(form -> form
+//                        .loginPage("/html/Loginpage/Login.html")
+//                        .defaultSuccessUrl("/html/Mainpage/Mainpage.html", true)
+//                        .permitAll()
+//                )
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(
+//                                "/",                     // 메인 접근 허용
+//                                "/html/Mainpage/Mainpage.html",   // 리다이렉트 대상 허용
+//                                "/html/Loginpage/Login.html",     // 로그인 페이지 허용
+//                                "/html/Signuppage/Signup.html",
+//                                "/css/**", "/js/**",
+//                                "/signUp",
+//                                "/signUp_pro"
+//                        ).permitAll()
+//                        .anyRequest().authenticated()
+//                );
+//        return http.build();
+//    }
 }
